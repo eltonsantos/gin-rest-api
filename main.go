86cf02d4d7d3e058d369a2dab9ffd4e2f7bc1ff2
Elton Santos <eltonaxl@hotmail.com>
2024-09-03 18:42:30 -0300

    Aluno by id

--- main.go
+++ main.go
@@ -2,15 +2,11 @@ package main
 
 import (
 	"github.com/eltonsantos/gin-rest-api/database"
-	"github.com/eltonsantos/gin-rest-api/models"
 	"github.com/eltonsantos/gin-rest-api/routes"
 )
 
 func main() {
 	database.ConectaComBancoDeDados()
-	models.Alunos = []models.Aluno{
-		{Nome: "Elton Santos", CPF: "123456789", RG: "111222333444555"},
-		{Nome: "Ana Santos", CPF: "78654321", RG: "999888777666555"},
-	}
+
 	routes.HandleRequest()
 }
