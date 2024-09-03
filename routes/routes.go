b343eb01821f14d6a414e74aa628b10c963057a6
Elton Santos <eltonaxl@hotmail.com>
2024-09-03 18:56:55 -0300

    Delete aluno

--- routes/routes.go
+++ routes/routes.go
@@ -11,5 +11,6 @@ func HandleRequest() {
 	r.GET("/:nome", controllers.Saudacao)
 	r.POST("/alunos", controllers.CriarNovoAluno)
 	r.GET("/alunos/:id", controllers.BuscaAlunoPorID)
+	r.DELETE("/alunos/:id", controllers.DeletaAluno)
 	r.Run()
 }
